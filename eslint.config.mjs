953e7e443959659bc0b616de82a4ba87fe26607d
Sera <me@ciderboi.xyz>
2025-04-11 23:28:55 +0530

    update eslint configuration to include TypeScript rules and enable warnings for unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["*.ts", "*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+    },
+  },
+  {
+    files: ["*.js", "*.jsx"],
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  }
 ];
 
 export default eslintConfig;
