143913c30620b2f19616532aff3c1cfeb5bb509b
Sera <me@ciderboi.xyz>
2025-04-15 20:06:55 +0530

    feat: switch from fetching songs data via API to reading from local JSON file

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,5 +1,6 @@
 import Music from "@/components/pages/Music";
-
+import fs from "fs/promises";
+import path from "path";
 import { Album, Track } from "@/components/ui/song-card";
 
 export type SongData = {
@@ -14,9 +15,12 @@ export type Props = {
 // server side query the songs from /data/songs.json
 const fetchData = async () => {
   // file is at /public/data/songs.json
-  const baseUrl = "https://cdn.ciderboi.xyz";
-  const res = await fetch(`${baseUrl}/data/songs.json`);
-  const data = await res.json();
+  // const baseUrl = "https://cdn.ciderboi.xyz";
+  // const res = await fetch(`${baseUrl}/data/songs.json`);
+  // const data = await res.json();
+
+  const file = await fs.readFile(path.join(process.cwd(), "public", "data", "songs.json"), "utf8");
+  const data = JSON.parse(file) as SongData;
 
   return {
     props: {
